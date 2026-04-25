46a58a287feb14cd77acd3d90b7ca3e0d6117484
jpulak <56491008+jpulak@users.noreply.github.com>
2026-04-24 19:24:29 -0500

    Update downloader.c

--- Jana/downloader.c
+++ Jana/downloader.c
@@ -1,82 +1,116 @@
-
-//implement of downloader
 #include "downloader.h"
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BUFFER_SIZE 1024
+#define CHUNK_SIZE 1024
 
-//set up tcp comms with server
-int download_file(const char* host, int port, const char* filename) 
+
+// ---------------- THREAD FUNCTION ----------------
+void* download_segment(void* arg)
 {
+    DownloadTask* task = (DownloadTask*)arg;
+
     int sockfd;
     struct sockaddr_in server_addr;
-    char buffer[BUFFER_SIZE];
-    int writing = 0; //flag 
+    char buffer[CHUNK_SIZE];
 
-    //creates tcp socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) 
-    {
-        perror("socket failed");
-        return -1; // if failed
-    }
+    if (sockfd < 0) pthread_exit(NULL);
 
-    //connect tracker or peer over tcp
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    inet_pton(AF_INET, host, &server_addr.sin_addr);
-    //call connect ot he t racker/peer
-    if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) 
-    {
-        perror("connect failed");
+    server_addr.sin_port = htons(task->port);
+    inet_pton(AF_INET, task->ip, &server_addr.sin_addr);
+
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         close(sockfd);
-        return -1;
+        pthread_exit(NULL);
     }
 
-    //send get request using protocol
-    pack_get(buffer, filename);
-    send(sockfd, buffer, strlen(buffer), 0);
+    long offset = task->start;
 
-    //open file and save content
-    FILE *fp = fopen("downloaded.txt", "w");
-    if(!fp) 
-    {
-        perror("fopen failed");
-        close(sockfd);
-        return -1;
+    while (offset < task->end) {
+
+        long chunk_end = offset + CHUNK_SIZE - 1;
+        if (chunk_end > task->end) chunk_end = task->end;
+
+        // build request
+        char req[256];
+        snprintf(req, sizeof(req),
+         "<GET %.180s %ld %ld>\n",
+         task->filename, offset, chunk_end);
+
+        send(sockfd, req, strlen(req), 0);
+
+        int n = recv(sockfd, buffer, CHUNK_SIZE, 0);
+        if (n <= 0) break;
+
+        // WRITE USING ALICES FUNCTION (IMPORTANT)
+        write_chunk(task->filename, offset, buffer, n);
+
+        offset += n;
     }
 
-    //read data from socket into buffer
-    //loop until connection close or no more data
-    while(1) 
-    {
-        memset(buffer, 0, BUFFER_SIZE);
-        int n = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
-        if(n <= 0) break;
-        buffer[n] = '\0';
-        // find protocol markers and execute accordingly
-        if(strstr(buffer, REP_GET_BEGIN) != NULL) 
-        {
-            writing = 1;
-            continue;
-        }
-
-        if(strstr(buffer, REP_GET_END) != NULL) 
-        {
-            break;
-        }
-
-        if(writing) 
-        {
-            fprintf(fp, "%s", buffer);
-        }
-    }
-    //clsoe file and socekt
-    fclose(fp);
     close(sockfd);
-    return 0; //success!
+
+    printf("[THREAD DONE] %s %ld-%ld from %s:%d\n",
+           task->filename, task->start, task->end,
+           task->ip, task->port);
+
+    pthread_exit(NULL);
+}
+
+
+// ---------------- MAIN COORDINATOR ----------------
+int multi_peer_download(const char* filename,
+                        PeerInfo* peers,
+                        int num_peers,
+                        long filesize,
+                        const char* state_file)
+{
+    pthread_t threads[num_peers];
+    DownloadTask tasks[num_peers];
+
+    long segment_size = filesize / num_peers;
+
+    for (int i = 0; i < num_peers; i++)
+    {
+        strcpy(tasks[i].filename, filename);
+        strcpy(tasks[i].ip, peers[i].ip);
+        tasks[i].port = peers[i].port;
+
+        tasks[i].start = i * segment_size;
+
+        if (i == num_peers - 1)
+            tasks[i].end = filesize;
+        else
+            tasks[i].end = (i + 1) * segment_size;
+
+        pthread_create(&threads[i], NULL, download_segment, &tasks[i]);
+    }
+
+    for (int i = 0; i < num_peers; i++)
+        pthread_join(threads[i], NULL);
+
+    printf("[INFO] All threads complete. Verifying file...\n");
+
+    // ---------------- MD5 CHECK (RODRIGO) ----------------
+    char* hash = compute_file_md5(filename);
+
+    if (hash) {
+        printf("[MD5] %s\n", hash);
+    } else {
+        printf("[MD5 ERROR]\n");
+    }
+
+    // ---------------- MERGE CHECK ----------------
+    merge_chunks(filename);
+
+    printf("[DONE] Download complete: %s\n", filename);
+
+    return 0;
 }
